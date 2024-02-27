71fa162f3bc6abe53dd7239fd0ddd6252da256c9
Dennis Elvir <dennis.elvir.code@gmail.com>
2024-02-26 18:28:42 -0600

    creadas la db,tablas,modelos,controllers

---
+++ database/migrations/2024_02_26_233213_create_vuelos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vuelos', function (Blueprint $table) {
+            $table->string('numeroVuelo', 5)->primary();
+            $table->string('origen', 10);
+            $table->string('destino', 10);
+            $table->integer('numeroAsientos');
+            $table->timestamp('Fecha_de_Salida');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vuelos');
+    }
+};
